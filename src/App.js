b28638ed2d714323f68df7490803ac75e43f642a
lchristian@unitedlocating.com-at-530555730387 <mtmusicandart@gmail.com>
2022-04-12 18:54:12 -0600

    nice

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import "./App.css";
 import Button from "./button";
 import Content from "./content";
+import React, { useState, useRef } from 'react'
 
 function App() {
+
+  const [toggle, setToggle] = useState(true)
+  const [isShow, setIsShow] = useState(true);
+
   return (
     <div className="App">
+
+      { isShow ?
+        <button id="assbtn" className="btn btn-success" onClick={() => setIsShow(!isShow)}>Hide</button> :   
+        <button  id="assbtn" className="btn btn-success" onClick={() => setIsShow(!isShow)}>Show</button>}
+
       <header className="App-header"></header>
       <div></div>
 
+      { isShow ? 
+
       <div className="video-overlay">
-        Bug Buck Bunny - Trailer
+       <h2> Bug Buck Bunny - Trailer</h2>
         <br />
-        <br />
-        <br />
-        <br />
-        <div>
+
+        <div id='text'>
           With our mobile games you can buy great CNFTs and get BCG tokens with
           them.🎮📱
         </div>
@@ -22,7 +32,18 @@ function App() {
         <Content></Content>
         <Content></Content>
         <Button></Button>
-      </div>
+
+
+        {toggle ? <button className="btn btn-success" onClick={() => setToggle(!toggle)}>Audio On</button> :
+
+                  <button className="btn btn-secondary" onClick={() => setToggle(!toggle)}>Audio Off</button> 
+    }
+      </div> : ''}
+
+
+      {/* { <button onClick={() => {alert("anus")}}>Hide</button> } */}
+
+      { isShow ? <>
 
       <img
         className="video1-overlay"
@@ -33,18 +54,20 @@ function App() {
         className="video3-overlay"
         src="https://cdn3.iconfinder.com/data/icons/capsocial-round/500/facebook-512.png"
         alt=""
-      />
+      /> </> : ''}
+
 
       <video
         className="fullscreen-video"
         id="myVideo"
         autoPlay="autoPlay"
-        muted
         loop
+        muted={toggle}
       >
         <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" />
         Your browser does not support the video tag.
       </video>
+      
     </div>
   );
 }
